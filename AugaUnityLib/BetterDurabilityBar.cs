0a5de1ea333a478f4cb39afbf3b7472e90e1ece8
Randy Knapp <randy.bravo2@gmail.com>
2021-04-20 13:53:52 -0700

    Durability Bar

--- AugaUnityLib/BetterDurabilityBar.cs
+++ AugaUnityLib/BetterDurabilityBar.cs
@@ -1,13 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace AugaUnity
 {
+    [RequireComponent(typeof(ItemTooltip))]
     public class BetterDurabilityBar : MonoBehaviour
     {
+        public Image Bar;
+
+        private ItemTooltip _itemTooltip;
+
+        public virtual void Awake()
+        {
+            _itemTooltip = GetComponent<ItemTooltip>();
+        }
+
+        public void LateUpdate()
+        {
+            var player = Player.m_localPlayer;
+            if (player == null || _itemTooltip == null)
+            {
+                return;
+            }
+
+            if (_itemTooltip.Item == null || !_itemTooltip.Item.m_shared.m_useDurability)
+            {
+                return;
+            }
+
+            var percent = _itemTooltip.Item.GetDurabilityPercentage();
+            Bar.fillAmount = percent;
+            Bar.color = percent <= 0.2f ? Color.red : Color.white;
+        }
     }
 }
