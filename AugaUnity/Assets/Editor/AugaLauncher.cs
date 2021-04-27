fd4b1e1d331ed9b7529ae78cb0e6c5f334f7d65e
Randy Knapp <randy.bravo2@gmail.com>
2021-04-27 07:56:33 -0700

    Updated Auga Launcher with more commands that help my workflow

--- AugaUnity/Assets/Editor/AugaLauncher.cs
+++ AugaUnity/Assets/Editor/AugaLauncher.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class AugaLauncher : EditorWindow
 {
     private const string ValheimPathPref = "Auga.ValheimPath";
-    [MenuItem("Mod/Auga Launcher")]
+    [MenuItem("Auga/Show Launcher Window")]
     public static void Init()
     {
         var window = (AugaLauncher)GetWindow(typeof(AugaLauncher));
@@ -16,27 +16,65 @@ public class AugaLauncher : EditorWindow
 
     public void OnGUI()
     {
+        EditorGUILayout.BeginHorizontal();
+
         var valheimPath = EditorPrefs.GetString(ValheimPathPref);
         valheimPath = EditorGUILayout.TextField("Valheim Path", valheimPath);
         EditorPrefs.SetString(ValheimPathPref, valheimPath);
 
-        if (GUILayout.Button(new GUIContent("Select Valheim Path")))
+        if (GUILayout.Button(new GUIContent("..."), GUILayout.Width(18)))
         {
-            valheimPath = Path.GetFullPath(EditorUtility.OpenFolderPanel("Select Location of valheim.exe", @"C:\Program Files (x86)\Steam\steamapps\common\Valheim\", ""));
+            var startPath = string.IsNullOrEmpty(valheimPath) ? @"C:\Program Files (x86)\Steam\steamapps\common\Valheim\" : valheimPath;
+            var newPath = EditorUtility.OpenFolderPanel("Select Location of valheim.exe", startPath, "");
+            if (!string.IsNullOrEmpty(newPath))
+            {
+                valheimPath = Path.GetFullPath(newPath);
+            }
             EditorPrefs.SetString(ValheimPathPref, valheimPath);
         }
 
-        if (GUILayout.Button(new GUIContent("Auga: Build & Launch"), GUILayout.Height(40)))
+        EditorGUILayout.EndHorizontal();
+        GUILayout.Space(4);
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button(new GUIContent("Build & Launch"), GUILayout.Height(40)))
         {
             BuildAndLaunch();
         }
+        if (GUILayout.Button(new GUIContent("Launch"), GUILayout.Height(40)))
+        {
+            LaunchValheim();
+        }
 
-        EditorUtility.ClearProgressBar();
+        EditorGUILayout.EndHorizontal();
+        GUILayout.Space(4);
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button(new GUIContent("Build Asset Bundle"), GUILayout.Height(40)))
+        {
+            BuildAssetBundles();
+        }
+        if (GUILayout.Button(new GUIContent("Deploy"), GUILayout.Height(40)))
+        {
+            DeployAssetFile();
+        }
+        if (GUILayout.Button(new GUIContent("Clear"), GUILayout.Height(40)))
+        {
+            RemoveDeployedAssetFile();
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        if (GUI.Button(position, "", GUIStyle.none))
+        {
+            GUI.FocusControl(null); 
+        }
     }
 
-    public void BuildAndLaunch()
+    [MenuItem("Auga/Build && Launch")]
+    public static void BuildAndLaunch()
     {
-        EditorUtility.DisplayProgressBar("Auga: Build & Launch", "Checking Valheim Path...", 0.1f);
+        EditorUtility.DisplayProgressBar("Auga", "Checking Valheim Path...", 0.1f);
         var valheimPath = EditorPrefs.GetString(ValheimPathPref);
         if (string.IsNullOrEmpty(valheimPath))
         {
@@ -44,18 +82,65 @@ public class AugaLauncher : EditorWindow
             EditorPrefs.SetString(ValheimPathPref, valheimPath);
         }
 
-        EditorUtility.DisplayProgressBar("Auga: Build & Launch", "Building Asset Bundles...", 0.3f);
-        Thread.Sleep(1000);
+        EditorUtility.DisplayProgressBar("Auga", "Building Asset Bundles...", 0.3f);
+        Thread.Sleep(500);
 
+        BuildAssetBundles();
+        DeployAssetFile();
+        LaunchValheim();
+    }
+
+    [MenuItem("Auga/Launch Valheim")]
+    public static void LaunchValheim()
+    {
+        EditorUtility.DisplayProgressBar("Auga", "Launching Valheim...", 1.0f);
+        var proc = new Process {
+            StartInfo = {
+                FileName = Path.Combine(GetValheimPath(), "valheim.exe"),
+                Arguments = "-console"
+            }
+        };
+        proc.Start();
+        Thread.Sleep(800);
+
+        EditorUtility.ClearProgressBar();
+    }
+
+    private static string GetStagePath()
+    {
         const string stageDirName = "AssetBundles";
         var projectRootDir = Path.Combine(Application.dataPath, "..");
-        var stagePath = Path.Combine(projectRootDir, stageDirName);
+        return Path.GetFullPath(Path.Combine(projectRootDir, stageDirName));
+    }
 
+    private static string GetValheimPath()
+    {
+        return EditorPrefs.GetString(ValheimPathPref);
+    }
+
+    private static string GetAssetBundleSourcePath()
+    {
+        return Path.GetFullPath(Path.Combine(GetStagePath(), "augaassets"));
+    }
+
+    private static string GetAssetBundleDestinationPath()
+    {
+        return Path.GetFullPath(Path.Combine(GetValheimPath(), "BepInEx/plugins/Auga/augaassets"));
+    }
+
+    [MenuItem("Auga/Build Asset Bundles")]
+    public static void BuildAssetBundles()
+    {
+        var stagePath = GetStagePath();
         BuildPipeline.BuildAssetBundles(stagePath, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
 
-        EditorUtility.DisplayProgressBar("Auga: Build & Launch", "Copying files...", 0.6f);
-        var sourceAssetBundle = Path.GetFullPath(Path.Combine(stagePath, "augaassets"));
-        var destAssetBundle = Path.GetFullPath(Path.Combine(valheimPath, "BepInEx/plugins/Auga/augaassets"));
+    [MenuItem("Auga/Deploy Asset Bundle")]
+    public static void DeployAssetFile()
+    {
+        EditorUtility.DisplayProgressBar("Auga: Build & Launch", "Deploying files...", 0.6f);
+        var sourceAssetBundle = GetAssetBundleSourcePath();
+        var destAssetBundle = GetAssetBundleDestinationPath();
         if (File.Exists(destAssetBundle))
         {
             FileUtil.ReplaceFile(sourceAssetBundle, destAssetBundle);
@@ -65,14 +150,21 @@ public class AugaLauncher : EditorWindow
             FileUtil.CopyFileOrDirectory(sourceAssetBundle, destAssetBundle);
         }
         Thread.Sleep(1000);
+        EditorUtility.ClearProgressBar();
+    }
 
-        EditorUtility.DisplayProgressBar("Auga: Build & Launch", "Launching Valheim...", 1.0f);
-        Thread.Sleep(1000);
+    [MenuItem("Auga/Clear Deployed Asset Bundle")]
+    public static void RemoveDeployedAssetFile()
+    {
+        EditorUtility.DisplayProgressBar("Auga: Build & Launch", "Clearing out deployed files...", 0.45f);
+        var destAssetBundle = GetAssetBundleDestinationPath();
 
-        var proc = new Process();
-        proc.StartInfo.FileName = Path.Combine(valheimPath, "valheim.exe");
-        proc.Start();
+        if (File.Exists(destAssetBundle))
+        {
+            FileUtil.DeleteFileOrDirectory(destAssetBundle);
+        }
 
+        Thread.Sleep(500);
         EditorUtility.ClearProgressBar();
     }
 }
