a47b029a1109245b8bc50bd7d7978b09de1ed2ef
Randy Knapp <randy.bravo2@gmail.com>
2021-05-06 19:42:22 -0700

    Making the EnemyHud work with CLLC

--- Auga/EnemeyHud_Setup.cs
+++ Auga/EnemeyHud_Setup.cs
@@ -1,4 +1,7 @@
-﻿using HarmonyLib;
+﻿using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace Auga
 {
@@ -11,6 +14,60 @@ namespace Auga
             public static bool Prefix(EnemyHud __instance)
             {
                 return !SetupHelper.DirectObjectReplace(__instance.transform, Auga.Assets.EnemyHud, "EnemyHud");
+            }
+        }
+
+        [HarmonyPatch(typeof(EnemyHud), nameof(EnemyHud.ShowHud))]
+        [HarmonyBefore("org.bepinex.plugins.creaturelevelcontrol")] // this doesn't seem to work?
+        public static class EnemyHud_ShowHud_Patch
+        {
+            public static void Postfix(EnemyHud __instance, Character c)
+            {
+                if (c == null || __instance.m_huds.TryGetValue(c, out EnemyHud.HudData _))
+                {
+                    return;
+                }
+
+                var hud = __instance.m_huds.LastOrDefault();
+                if (hud.Key != null && hud.Value != null)
+                {
+                    const int maxLevelForStarDisplays = 6;
+                    const int firstStarDisplayLevel = 2;
+                    const int lastStarDisplayLevel = 3;
+
+                    var level = c.GetLevel();
+                    if (level > lastStarDisplayLevel)
+                    {
+                        var hudGui = hud.Value.m_gui;
+                        for (var i = firstStarDisplayLevel; i <= maxLevelForStarDisplays; i++)
+                        {
+                            var levelDisplay = hudGui.transform.Find($"level_{level}");
+                            if (levelDisplay != null)
+                            {
+                                levelDisplay.gameObject.SetActive(i == level);
+                            }
+                        }
+
+                        var levelDisplayX = hudGui.transform.Find("level_X");
+                        if (levelDisplayX)
+                        {
+                            var useExtendedLevel = level > maxLevelForStarDisplays;
+                            if (useExtendedLevel)
+                            {
+                                var levelXDisplayName = $"level_{level}";
+                                var newLevelXDisplay = hudGui.transform.Find(levelXDisplayName);
+                                if (newLevelXDisplay == null)
+                                {
+                                    newLevelXDisplay = Object.Instantiate(levelDisplayX, levelDisplayX.parent, false);
+                                    newLevelXDisplay.name = levelXDisplayName;
+                                }
+
+                                var text = levelDisplayX.GetComponentInChildren<Text>();
+                                text.text = $"x {level - 1}";
+                            }
+                        }
+                    }
+                }
             }
         }
     }
