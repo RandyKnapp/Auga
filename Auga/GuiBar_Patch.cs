21b527fbec01ce32f22e5100bf38f3e1d6be6c02
Randy Knapp <randy.bravo2@gmail.com>
2021-05-17 20:34:10 -0700

    Crosshair, Piece Health bar, Action Progress bar, Stealth

---
+++ Auga/GuiBar_Patch.cs
@@ -0,0 +1,20 @@
+﻿using HarmonyLib;
+using UnityEngine.UI;
+
+namespace Auga
+{
+    [HarmonyPatch(typeof(GuiBar), nameof(GuiBar.SetBar))]
+    public static class GuiBar_Patch
+    {
+        public static bool Prefix(GuiBar __instance, float i)
+        {
+            if (__instance.m_barImage.type == Image.Type.Filled)
+            {
+                __instance.m_barImage.fillAmount = i;
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
