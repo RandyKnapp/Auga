25232d5ee4a823a875c29a7dc1ab6665c664f60b
Randy Knapp <randy.bravo2@gmail.com>
2021-05-24 10:02:06 -0700

    Credits

--- Auga/MainMenu_Setup.cs
+++ Auga/MainMenu_Setup.cs
@@ -37,17 +37,22 @@ namespace Auga
             __instance.m_versionLabel = mainMenu.Find("Version").GetComponent<Text>();
             __instance.m_betaText = mainMenu.Find("DummyObjects/Dummy").gameObject;
             __instance.m_ndaPanel = mainMenu.Find("DummyObjects/Dummy").gameObject;
-            UpdateButtonListener(__instance.m_mainMenu.transform, "MenuList/StartGame", __instance.OnStartGame);
-            UpdateButtonListener(__instance.m_mainMenu.transform, "MenuList/Settings", __instance.OnButtonSettings);
-            UpdateButtonListener(__instance.m_mainMenu.transform, "MenuList/Credits", __instance.OnCredits);
-            UpdateButtonListener(__instance.m_mainMenu.transform, "MenuList/Exit", __instance.OnAbort);
+            SetButtonListener(__instance.m_mainMenu.transform, "MenuList/StartGame", __instance.OnStartGame);
+            SetButtonListener(__instance.m_mainMenu.transform, "MenuList/Settings", __instance.OnButtonSettings);
+            SetButtonListener(__instance.m_mainMenu.transform, "MenuList/Credits", __instance.OnCredits);
+            SetButtonListener(__instance.m_mainMenu.transform, "MenuList/Exit", __instance.OnAbort);
             __instance.GetComponentInChildren<ChangeLog>(true).m_changeLog = originalChangeLogAsset;
 
             var connectionFailedDialog = __instance.Replace("ConnectionFailed", Auga.Assets.MainMenuPrefab);
             __instance.m_connectionFailedPanel = connectionFailedDialog.gameObject;
             __instance.m_connectionFailedError = connectionFailedDialog.Find("Text").GetComponent<Text>();
 
-            var black = __instance.Replace("BLACK", Auga.Assets.MainMenuPrefab);
+            var credits = __instance.Replace("Credits", Auga.Assets.MainMenuPrefab);
+            __instance.m_creditsPanel = credits.gameObject;
+            __instance.m_creditsList = (RectTransform)credits.Find("ContactInfo");
+            SetButtonListener(__instance.m_creditsPanel.transform, "Back-panel/ButtonSettings", __instance.OnCreditsBack);
+
+            __instance.Replace("BLACK", Auga.Assets.MainMenuPrefab);
 
             __instance.m_loading = __instance.Replace("Loading", Auga.Assets.MainMenuPrefab).gameObject;
             __instance.m_loading.SetActive(false);
@@ -58,7 +63,7 @@ namespace Auga
             Localization.instance.Localize(__instance.transform);
         }
 
-        private static void UpdateButtonListener(Transform root, string childName, UnityAction listener)
+        private static void SetButtonListener(Transform root, string childName, UnityAction listener)
         {
             var button = root.Find(childName).GetComponent<Button>();
             button.onClick = new Button.ButtonClickedEvent();
