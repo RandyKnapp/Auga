02ac44288b92872de37d7fea8bfd173f62076f77
Randy Knapp <randy.bravo2@gmail.com>
2021-07-27 22:22:02 -0700

    Removed unused common code from the API

--- Auga/API.Common.cs
+++ Auga/API.Common.cs
@@ -3,19 +3,12 @@ using UnityEngine.UI;
 
 namespace Auga
 {
-    public enum TooltipObjectBackgroundType
-    {
-        Item, Diamond, Skill
-    }
-
     public enum RequirementWireState
     {
         Absent,
         Have,
         DontHave
     }
-
-    public delegate void TooltipCreationDelegate(GameObject complexTooltipGO, ItemDrop.ItemData item);
 
     public class WorkbenchTabData
     {
