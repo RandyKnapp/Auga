a38b1b3067a583877d31b2264d9dda5827805f82
Pete Navarra <vapokrocks@gmail.com>
2023-04-02 17:28:37 -0400

    Minimap, Map, Inventory, and TextInput changes

--- Auga/TextInput_Setup.cs
+++ Auga/TextInput_Setup.cs
@@ -13,24 +13,5 @@ namespace Auga
                 return !SetupHelper.DirectObjectReplace(__instance.transform, Auga.Assets.TextInput, "TextInput");
             }
         }
-
-        [HarmonyPatch(typeof(TextInput), nameof(TextInput.Update))]
-        [HarmonyPrefix]
-        public static bool Update_Prefix(TextInput __instance)
-        {
-            __instance.m_visibleFrame = TextInput.m_instance.m_panel.gameObject.activeSelf;
-            if (!__instance.m_visibleFrame || Console.IsVisible() || Chat.instance.HasFocus())
-            {
-                return true;
-            }
-
-            if (ZInput.GetButtonDown("Inventory"))
-            {
-                __instance.Hide();
-                return false;
-            }
-
-            return true;
-        }
     }
 }
