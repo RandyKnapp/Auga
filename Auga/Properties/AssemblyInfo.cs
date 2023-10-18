f0e9927a7c3e1589597f2ee9462221a291f55926
Pete Navarra <vapokrocks@gmail.com>
2023-10-18 02:58:03 -0400

    Release 1.3.2

--- Auga/Properties/AssemblyInfo.cs
+++ Auga/Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.4")]
 [assembly: AssemblyFileVersion("1.4")]
 #else
-[assembly: AssemblyVersion("1.3.1")]
-[assembly: AssemblyFileVersion("1.3.1")]
+[assembly: AssemblyVersion("1.3.2")]
+[assembly: AssemblyFileVersion("1.3.2")]
 #endif
