3fe32e757c801fc4dbf1dba7b41b6aea2de9549b
Pete Navarra <vapokrocks@gmail.com>
2023-10-19 03:58:02 -0400

    Release 1.3.3

--- Auga/Auga.cs
+++ Auga/Auga.cs
@@ -84,7 +84,7 @@ namespace Auga
     public class Auga : BaseUnityPlugin
     {
         public const string PluginID = "randyknapp.mods.auga";
-        public const string Version = "1.3.2";
+        public const string Version = "1.3.3";
 
         public enum StatBarTextDisplayMode { JustValue, ValueAndMax, ValueMaxPercent, JustPercent }
         public enum StatBarTextPosition { Off = -1, Above, Below, Center, Start, End };
--- Auga/Properties/AssemblyInfo.cs
+++ Auga/Properties/AssemblyInfo.cs
@@ -39,6 +39,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.4")]
 [assembly: AssemblyFileVersion("1.4")]
 #else
-[assembly: AssemblyVersion("1.3.2")]
-[assembly: AssemblyFileVersion("1.3.2")]
+[assembly: AssemblyVersion("1.3.3")]
+[assembly: AssemblyFileVersion("1.3.3")]
 #endif
