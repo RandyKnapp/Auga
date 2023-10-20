b485dd85b473f70a0babfea46aad640650c8b548
Pete Navarra <vapokrocks@gmail.com>
2023-10-19 20:53:06 -0400

    Improved Barber UI

--- AugaUnityLib/AugaBarberController.cs
+++ AugaUnityLib/AugaBarberController.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace AugaUnity
 {
     public class AugaBarberController : MonoBehaviour
     {
-        public Button HairLeft;
-        public Button HairRight;
-        public Button BeardLeft;
-        public Button BeardRight;
-        public Button Apply;
-        public Button Close;
-        public Slider HairTone;
-        public Slider Blondeness;
+        public Button HairRightButton;
+        public Button HairLeftButton;
+        public Button BeardRightButton;
+        public Button BeardLeftButton;
+
+        private bool _playerFound;
         
         private PlayerCustomizaton _playerCustomizaton;
         private void Awake()
@@ -23,19 +20,45 @@ namespace AugaUnity
 
         private void Start()
         {
-            HairLeft.onClick.AddListener(_playerCustomizaton.OnHairLeft);
-            HairRight.onClick.AddListener(_playerCustomizaton.OnHairRight);
-            BeardLeft.onClick.AddListener(_playerCustomizaton.OnBeardLeft);
-            BeardRight.onClick.AddListener(_playerCustomizaton.OnBeardRight);
-            Apply.onClick.AddListener(_playerCustomizaton.OnApply);
-            Close.onClick.AddListener(_playerCustomizaton.OnCancel);
-            
             _playerCustomizaton.m_beards = ObjectDB.instance.GetAllItems(ItemDrop.ItemData.ItemType.Customization, "Beard");
             _playerCustomizaton.m_hairs = ObjectDB.instance.GetAllItems(ItemDrop.ItemData.ItemType.Customization, "Hair");
-            _playerCustomizaton.m_beards.RemoveAll((Predicate<ItemDrop>) (x => x.name.Contains("_")));
-            _playerCustomizaton.m_hairs.RemoveAll((Predicate<ItemDrop>) (x => x.name.Contains("_")));
-            _playerCustomizaton.m_beards.Sort((Comparison<ItemDrop>) ((x, y) => Localization.instance.Localize(x.m_itemData.m_shared.m_name).CompareTo(Localization.instance.Localize(y.m_itemData.m_shared.m_name))));
-            _playerCustomizaton.m_hairs.Sort((Comparison<ItemDrop>) ((x, y) => Localization.instance.Localize(x.m_itemData.m_shared.m_name).CompareTo(Localization.instance.Localize(y.m_itemData.m_shared.m_name))));
+            _playerCustomizaton.m_beards.RemoveAll(x => x.name.Contains("_"));
+            _playerCustomizaton.m_hairs.RemoveAll(x => x.name.Contains("_"));
+            _playerCustomizaton.m_beards.Sort((x, y) => Localization.instance.Localize(x.m_itemData.m_shared.m_name).CompareTo(Localization.instance.Localize(y.m_itemData.m_shared.m_name)));
+            _playerCustomizaton.m_hairs.Sort((x, y) => Localization.instance.Localize(x.m_itemData.m_shared.m_name).CompareTo(Localization.instance.Localize(y.m_itemData.m_shared.m_name)));
+        }
+
+        private void Update()
+        {
+            if (!_playerFound)
+            {
+                if (Player.m_localPlayer == null)
+                    return;
+                _playerFound = true;
+            }
+            
+            var hairSetting = _playerCustomizaton.GetHairIndex();
+            var beardSetting = +_playerCustomizaton.GetBeardIndex();
+
+            if (hairSetting == 0)
+                HairLeftButton.gameObject.SetActive(false);
+            else if (hairSetting >= (_playerCustomizaton.m_hairs.Count -1))
+                HairRightButton.gameObject.SetActive(false);
+            else
+            {
+                HairLeftButton.gameObject.SetActive(true);
+                HairRightButton.gameObject.SetActive(true);
+            }
+                
+            if (beardSetting == 0)
+                BeardLeftButton.gameObject.SetActive(false);
+            else if (beardSetting >= (_playerCustomizaton.m_beards.Count -1))
+                BeardRightButton.gameObject.SetActive(false);
+            else
+            {
+                BeardLeftButton.gameObject.SetActive(true);
+                BeardRightButton.gameObject.SetActive(true);
+            }
         }
     }
 }
